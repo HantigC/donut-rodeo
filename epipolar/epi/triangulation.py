a5490c51276e60b01c6152947fbd975662637dee
HantigC <cristianhantig96@gmail.com>
2024-03-23 12:09:53 +0200

    add linear triangulation for two cameras

--- epipolar/epi/triangulation.py
+++ epipolar/epi/triangulation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from epi import geometry as geom
 
 
 def to_2d(x: np.ndarray):
@@ -20,6 +21,7 @@ def mid_point(
     r: np.ndarray,
     q: np.ndarray,
     s: np.ndarray,
+    return_on_rays: bool = False,
 ) -> np.ndarray:
     p = to_2d(p)
     r = to_2d(r)
@@ -40,4 +42,30 @@ def mid_point(
     P = p + r * sigma[:, np.newaxis]
     Q = q + s * miu[:, np.newaxis]
     H = (P + Q) / 2
+    if return_on_rays:
+        return H, P, Q
     return H
+
+
+class _Linear:
+
+    @staticmethod
+    def two_cameras(
+        points1: np.ndarray,
+        points2: np.ndarray,
+        cam1: np.ndarray,
+        cam2: np.ndarray,
+    ) -> np.ndarray:
+        points = np.stack([points1, points2], axis=1)
+        cams = np.stack([cam1, cam2])
+
+        sistems = points[..., np.newaxis] * cams[:, 2, np.newaxis] - cams[:, :2]
+        sistems = sistems.reshape(-1, 4, 4)
+
+        triangulated_points = np.linalg.svd(sistems)
+        triangulated_points = triangulated_points.Vh[:, 3, :]
+        triangulated_points = geom.from_homogenous(triangulated_points, axis=1)
+        return triangulated_points
+
+
+linear = _Linear
